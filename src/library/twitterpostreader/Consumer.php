837b64d094d7aaf06063b65f342b13c08549c4c0
Lucas A. de Araújo <lucasdearaujo@icloud.com>
2014-06-04 13:52:39 -0300

    New folder configuration

---
+++ src/library/twitterpostreader/Consumer.php
@@ -0,0 +1,41 @@
+<?php namespace TwitterPostReader;
+
+/**
+ * The twitter consumer configuration
+ * @author Lucas A. de Araújo <lucasdearaujo@icloud.com>
+ */
+class Consumer {
+
+	/**
+	 * The consumer key
+	 * @var string
+	 */
+	public $key;
+
+	/**
+	 * The consumer secret
+	 * @var string
+	 */
+	public $secret;
+
+	/**
+	 * Set the consumer key
+	 * @param string $key Consumer key
+	 */
+	public function setKey($key){
+		$this->key = $key;
+	}
+
+	/**
+	 * Return the consumer key
+	 * @return string
+	 */
+	public function getKey(){
+		return $this->key;
+	}
+
+	public function 
+
+}
+
+?>
